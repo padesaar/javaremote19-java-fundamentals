13ad7f7d6b68a2ab30edbaa71665e8172cd5dece
Kätlin <kpadesaar@gmail.com>
2022-09-21 08:16:56 +0300

    Homeeork2 added

--- src/Homework2.java
+++ src/Homework2.java
@@ -4,10 +4,44 @@
  * @author Kätlin Padesaar-Korela
  */
 
+//if(i > y) {
+//            System.out.println("i is greater than y");
+//        } else {
+//            System.out.println("y is greater than i");
+//
+//        }
+//
+//
+//          /*
+//        if(shirt size M && shirt colour == black {
+//        //buy the shirt
+//       } else if (shirt size == M && shirt color == blue) {
+//       // buy the shirt
+//
+//        } else {
+//        // buy the shirt from other shop
+//           */
+//
+//
+//        if(i > y) { //if you need to check multiple conditions
+//            System.out.println("i is greater than y");
+//
+//    } else if(i < 9) {
+//        System.out.println("i is lesser");
+//
+//        } else {
+//        System.out.println("i is invalid");
+//    }
+
 public class Homework2 {
 
     public static void main(String[] args) {
 
+    int y = 15;
+    int x = 25;
 
+    if (y < x){
+        System.out.println("y moves to white bucket");
+    }
     }
 }
