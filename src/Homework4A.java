8b02b392907c84a1681632e30867e45763b517dc
Kätlin <kpadesaar@gmail.com>
2022-09-21 21:33:48 +0300

    Homework 4A added

---
+++ src/Homework4A.java
@@ -0,0 +1,28 @@
+/**
+ * Homework for Arrays class
+ *
+ * @author Kätlin Padesaar-Korela
+ */
+
+public class Homework4A {
+    public static void main(String[] args) {
+
+
+
+        // Homework4
+        // to find the smallest element in the array b
+        // second homework in slack
+
+
+
+        int result2 = b[1]; //27
+        for(int  y: b){
+            if(y < result2) {
+                result2 = y;
+            }
+
+
+        }
+        System.out.println(result2);
+    }
+}
