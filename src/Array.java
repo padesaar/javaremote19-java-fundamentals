f87856fec485461fd343e3a1b3d71c2df7740ff0
Kätlin <kpadesaar@gmail.com>
2022-09-22 19:58:14 +0300

    Added 2d and 3d arrays

--- src/Array.java
+++ src/Array.java
@@ -63,9 +63,9 @@ public class Array {
         // find the largest element in the array
         // Brute-force method
         int result = b[0]; //9
-        for(int  x: b){
-            if(x > result) {
-                result = x;
+        for(int  f: b){
+            if(f > result) {
+                result = f;
             }
         }
 
@@ -86,7 +86,75 @@ public class Array {
     }
         System.out.println(result2);
 
+        // Multi-dimensional array
 
+        // int [][] - 2D array
+
+
+        int[][] array2d = new int[3][3]; // this can hold 9 values, 3 rows and 3 columns
+        // 3 is x value and 3 is y value
+        // we have only 1 table (vaata 3D)
+
+        //row 1
+        array2d[0][0] = 1;
+        array2d[0][1] = 0;
+        array2d[0][2] = 1;
+
+        //row 2
+
+        array2d[1][0] = 2;
+        array2d[1][1] = 10;
+        array2d[1][2] = 9;
+
+        //row 3
+
+        array2d[2][0] = 4;
+        array2d[2][1] = 9;
+        array2d[2][2] = 8;
+
+        int[][] array2dver2 = { {1, 0, 1}, {2, 10, 9}, {4, 9, 8}};
+
+        for(int x = 0; x < 3; x++) {
+          for(int y = 0; y < 3; y++) {
+                System.out.print(array2dver2[x][y] + " ");
+        }
+            System.out.println();
+        }
+
+       // 3 dimensional array 3D (1D + 2D array)
+        // 1D represents number of tables
+        // 2D represents array with rows and columns
+
+        int[][][] array3d = new int[3][3][3]; // [] esimene nt mitu tabelit on, teine mitu rows ja kolmas mitu columns are
+        //table 1
+        // row 1
+        array3d[0][0][0] = 1;
+        array3d[0][0][1] = 0;
+        array3d[0][0][2] = 1;
+
+        array3d[2][1][2] = 7;
+
+
+        // there are brackets for each table and each row
+        int[][][] array3dver2 = { {
+                {1, 0, 1}, {1, 8, 0}, {2, 5, 7} },
+                { {9, 8, 7}, {5, 8, 9}, {3, 5, 0}},
+                { {5, 6, 2}, {4, 5, 7}, {2, 6, 8}}
+        };
+
+
+        System.out.println("\n");
+        for(int w = 0; w < 3; w++) {
+            for(int s = 0; s < 3; s++) {
+                for(int g = 0; g < 3; g++) {
+                System.out.print(array3dver2[w][s][g]+ "\t");
+
+            }
+            System.out.println();
+        }
+        System.out.println("\n\n");
+
+        }
 }
 
 }
