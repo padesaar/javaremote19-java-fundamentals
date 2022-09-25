9943c38fc4c993948b07f41b29b495900eece509
Kätlin <kpadesaar@gmail.com>
2022-09-25 18:11:43 +0300

    Added Homework5

---
+++ src/Homework5.java
@@ -0,0 +1,34 @@
+/**
+ * Homework5
+ *
+ * @author Kätlin Padesaar-Korela
+ */
+
+
+public class Homework5 {
+    public static void main(String[] args) {
+
+
+        // First task: Write a Java program to print all odd numbers from array [1, 7, 3, 10, 9]
+
+        int[]num = {1, 7, 3, 10, 9 };
+
+
+
+       for (int i= 0; i < num.length; i++) {
+
+          if (num[i] % 2 != 0 ) {
+              System.out.println(num[i]);
+
+           }
+
+       }
+
+
+       // Task 2: Write a Java program to find the number of even and odd integers form an array of integers [1, 7, 3, 10, 9]
+
+
+
+
+    }
+}
